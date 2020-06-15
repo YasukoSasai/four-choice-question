086c1e14b50ee82e1a4789d19292ebe2c8a0c006
Yasuko Sasai <>
2020-06-15 23:26:14 +0900

    routerやっぱりやめた

--- yasuko_question/src/components/App/Header.vue
+++ yasuko_question/src/components/App/Header.vue
@@ -1,21 +1,16 @@
 <template>
   <div>
     <h1>G検定過去問トレーニング</h1>
-    <!-- <div class="items"> -->
-      <v-btn>G検定とは</v-btn>
+      <v-btn>Home ーG検定とはー</v-btn>
       <v-btn>練習問題</v-btn>
       <v-btn>掲示板</v-btn>
       <v-btn>Somethimg</v-btn>
-      <!-- <GTest></GTest> -->
-    <!-- </div> -->
   </div>
 </template>
 
 <script>
-// import GTest from "./GTest.vue";
 export default {
   components:{
-    // GTest
   }
 }
 </script>
