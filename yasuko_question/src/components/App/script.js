a3147491ef8093c1b2a09f37d65b14d51fad6446
Yasuko Sasai <>
2020-06-23 20:57:41 +0900

    firedatabase

--- yasuko_question/src/components/App/script.js
+++ yasuko_question/src/components/App/script.js
@@ -1,9 +1,12 @@
 import Header from "@/components/Header/Index.vue";
 import Footer from "@/components/Footer/Index.vue";
 
+
+
 export default {
   components:{
     Header,
     Footer
   }
+  
 };
--- yasuko_question/src/main.js
+++ yasuko_question/src/main.js
@@ -4,7 +4,25 @@ import Vuetify from 'vuetify'
 import "vuetify/dist/vuetify.min.css";
 import router from './router'
 
+import firebase from 'firebase'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA19l8pnb8Fk1e_1iKqVV4ibqJFf1VvNqs",
+  authDomain: "y-question.firebaseapp.com",
+  databaseURL: "https://y-question.firebaseio.com",
+  projectId: "y-question",
+  storageBucket: "y-question.appspot.com",
+  messagingSenderId: "898183518714",
+  appId: "1:898183518714:web:0ce832a5c3092ea6bc6c43"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 Vue.use(Vuetify)
+
+export const db = firebase.firestore()
 
 Vue.config.productionTip = false;
 
